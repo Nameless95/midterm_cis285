2827ebbc0030ed20f412e85f5e43e9e89880a160
NamelessGod95 <jaxonpecora@gmail.com>
2022-03-10 14:32:28 -0500

    fixing

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,16 +1,9 @@
 public class AddIntegers {
-
     public static void main(String[] args) {
-
         int first = 15;
         int second = 25;
-
-    
-
-      // calculate sum value
-      sum = num1 + num2;
-
-      // display the sum value
-      System.out.println("Sum = " + sum);
-   }
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+        System.out.println("The sum is: " + sum);
+    }
 }
